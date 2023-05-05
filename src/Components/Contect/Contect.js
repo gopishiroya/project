3978aa02ace5bed0debdb25297b96caa4491a6da
gopishiroya <gopishiroya63@gmail.com>
2023-05-05 14:19:44 +0530

    a

--- src/Components/Contect/Contect.js
+++ src/Components/Contect/Contect.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 
 const Contect = () => {
   return (
     <div>
       contect
+      <Header />
+      <Footer />
     </div>
   )
 }
