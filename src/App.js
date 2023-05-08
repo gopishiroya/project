0ad00d6c1e698eb460c0495eb05f235417b611da
ektapatel1612 <ektarupareliya11@gmail.com>
2023-05-08 10:18:47 +0530

    app

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import Home from './Components/Home/Home';
 import { Route, Routes } from 'react-router-dom';
 import About from './Components/About/About';
-<<<<<<< HEAD
+
 import { Deshboard } from './Admin/Deshboard/Deshboard';
 // import Menu from './Components/Menu/Menu';
-=======
 import Menu from './Components/Menu/Menu';
 import Contect from './Components/Contect/Contect';
 import Login from './Components/Login/Login';
 import Register from './Components/Register/Register';
->>>>>>> 6f0f1fe4de2373a965608a337116100a7a9f966f
+
 
 function App() {
   return (
@@ -18,15 +17,14 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />}/>
-<<<<<<< HEAD
+
         {/* <Route path='/menu' element={<Menu />}/> */}
         <Route path='/deshboard' element={<Deshboard />} />
-=======
+
         <Route path='/menu' element={<Menu />}/>
         <Route path='/contect' element={<Contect />} />
         <Route path="/login" element={<Login />}/>
         <Route path='/register' element={<Register />} />
->>>>>>> 6f0f1fe4de2373a965608a337116100a7a9f966f
       </Routes>
     </div>
   );
