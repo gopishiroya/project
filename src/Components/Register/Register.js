ed3b3638e5344778f1c69fa00912000668d0353d
gopishiroya <gopishiroya63@gmail.com>
2023-05-11 17:26:17 +0530

    11/05

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,5 +1,3 @@
-
-import React, { useState } from "react";
 import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -9,8 +7,7 @@ import "./Register.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { registerInitaiate } from "../../Action/Action";
 import { ToastContainer, toast } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -32,7 +29,7 @@ const Register = () => {
   function handleRegister(e) {
     e.preventDefault();
     if (password !== cpassword) {
-      return;
+      return toast.error("please enter correct password");
     }
     dispatch(registerInitaiate(email, password));
     setName("");
@@ -43,87 +40,84 @@ const Register = () => {
     toast.success("data added success");
   }
 
-
   return (
-    <div className="register">
-      <div>
-        <Header />
+    <>
+      <div className="register">
+        <div>
+          <Header />
+        </div>
+        <div className="form">
+          <Form className="form1">
+            <Typography.Title className="formtitle">
+              REGISTER NOW
+            </Typography.Title>
+            <div className="inputfield">
+              <Input
+                className="input"
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <Input
+                className="input"
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <Input
+                className="input"
+                type="text"
+                placeholder="Enter your number"
+                maxLength={10}
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+                required
+              />
+              <Input.Password
+                className="input"
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <Input.Password
+                className="input"
+                type="password"
+                placeholder="Confirm your password"
+                value={cpassword}
+                onChange={(e) => setCpassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="registerbutton">
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="registerbtn"
+                onClick={handleRegister}
+              >
+                Register Now
+              </Button>
+            </div>
+            <Typography.Paragraph className="lparagraph">
+              already have an account?{" "}
+              <Link className="llink" to="/login">
+                login now
+              </Link>
+            </Typography.Paragraph>
+          </Form>
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
-      <div className="form">
-        <Form className="form1">
-          <Typography.Title className="formtitle">
-            REGISTER NOW
-          </Typography.Title>
-          <div className="inputfield">
-            <Input
-              className="input"
-              type="text"
-              placeholder="Enter your name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-            <Input
-              className="input"
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-            <Input
-              className="input"
-              type="text"
-              placeholder="Enter your number"
-              maxLength={10}
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
-              required
-
-            />
-            <Input.Password
-              className="input"
-              type="password"
-              placeholder="Enter your password"
-
-             
-
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-
-            />
-            <Input.Password
-              className="input"
-              type="password"
-              placeholder="Confirm your password"
-              value={cpassword}
-              onChange={(e) => setCpassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="registerbutton">
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="registerbtn"
-              onClick={handleRegister}
-            >
-              Register Now
-            </Button>
-          </div>
-          <Typography.Paragraph className="lparagraph">
-            already have an account?{" "}
-            <Link className="llink" to="/login">
-              login now
-            </Link>
-          </Typography.Paragraph>
-        </Form>
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </div>
+      <ToastContainer theme="dark" />
+    </>
   );
 };
 
